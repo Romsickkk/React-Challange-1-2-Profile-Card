acf539fac26e29f1448dc9802e8c0569f2a069d2
Romsickkk <arman551@mail.ru>
2024-10-10 19:14:42 +0400

    new file:   public/img/RomaIMG.jpg 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const skillsData = [
+  {
+    skill: "HTML",
+    level: "advanced",
+    color: "#ff5e00",
+  },
+  {
+    skill: "CSS",
+    level: "intermediate",
+    color: "#61dafb",
+  },
+  {
+    skill: "JavaScript ",
+    level: "advanced",
+    color: "#ffdd00",
+  },
+  {
+    skill: "Bootstrap",
+    level: "intermediate",
+    color: "#8c00ff",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#0072bc",
+  },
+  {
+    skill: "Redux",
+    level: "beginner",
+    color: "#006cff",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#000000",
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const emojy = {
+  advanced: "😎",
+  intermediate: "👍",
+  beginner: "✌",
+};
+
+function App() {
+  return (
+    <div className="mainBox">
+      <Description
+        name="Roman Babayan"
+        aboutMe="My name is Roma and i'm learning React and developing"
+        photoName="img/RomaIMG.jpg"
+      />
+    </div>
+  );
+}
+
+function Description(props) {
+  return (
+    <div className="discroption">
+      <img className="faceImg" src={props.photoName} alt="" />
+      <div className="me">
+        <h1 className="name">{props.name}</h1>
+        <h4 className="aboutMe">{props.aboutMe}</h4>
+      </div>
+      <Skills />
+    </div>
+  );
+}
+
+function Skills() {
+  return (
+    <div className="skills">
+      {skillsData.map((skill, index) => (
+        <div
+          key={index}
+          className="skill"
+          style={{
+            background: `linear-gradient(145deg, ${skill.color}, #e0e0e0)`,
+          }}
+        >
+          {skill.skill} {emojy[skill.level]}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
